37b6f4084dcbe275e75000d854e50b6671fb9055
jpbandrade <jpbatistandrade@gmail.com>
2022-06-15 11:18:00 +0100

    second

---
+++ OpStrings/mod1.py
@@ -0,0 +1,3 @@
+def meanString(1frase,2frase):
+    print(f"A media do tamanho das palavras é: {((len(1frase)+len(2frase))/2)}")
+
---
+++ OpStrings/mod2.py
@@ -0,0 +1,11 @@
+def maxLen(1frase,2frase):
+    if len(1frase)>len(frase):
+        print(f"A primeira String é maior que: {len(1frase)}")
+    else:
+        print(f"A segunda String é maior que:{len(2frase)} ")
+
+def minLen(1frase,2frase):
+    if len(1frase)<len(2frase):
+        print(f"A primeira String é menor que: {len(1frase)}")
+    else:
+        print(f"A segunda String é menor que: {len(2frase)}")
